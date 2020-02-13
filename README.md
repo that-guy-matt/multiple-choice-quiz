8953a57b16d04dc8856ba73ef8bf67968d365883
Matthew <matteidsen@gmail.com>
2020-02-12 22:52:02 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Multiple Choice Quiz App
+
+This was a homework assignment for a JavaScript class
+
+## About the project
+
+The requirement for this homework assignment was to create a quiz application that made use of an array and/or object value.
+
+## Built with
+
+- HTML
+- JavaScript
+- JQuery (upcoming)
+
+## Roadmap
+
+Currently the quiz is just functional. It goes through a set loop of 10 questions and keeps score of one point per question. There is no css styling at all at this point other than just to make a class to hide certain objects that will not be produced until the game end screen. The current plan is as follows:
+
+1. Recreate the game using JQuery (per homework assignment)
+2. Add some CSS styling because, let's be honest, it's kinda ugly
+3. (Optional) create a randomized array of the questions so they are not always in the same order
+
+## Known issues
+
+1. Play again button functionality
+
+- On subsequent playthroughs, the game would crash due to not getting the right question count increment. It jumps from 0 to the end after only a single question. I have no idea why it's doing that.
+- Currently this has been hotfixed by making the button do a page reload instead of resetting the game and variables.
---
+++ game.js
@@ -0,0 +1,193 @@
+// constants
+const QUESTION = document.getElementById("question");
+const SCORE = document.getElementById("score");
+const SCORECONT = document.getElementById("scoreContainer");
+const RESTART = document.getElementById("restart");
+const CHOICEA = document.getElementById("a");
+const CHOICEB = document.getElementById("b");
+const CHOICEC = document.getElementById("c");
+const CHOICED = document.getElementById("d");
+
+const QUESTIONS = [
+  {
+    question: "Which is the smallest planet in our solar system?",
+    answers: {
+      a: "Earth",
+      b: "Pluto",
+      c: "Mercury",
+      d: "Venus"
+    },
+    correct: "c"
+  },
+  {
+    question: "What is the largest type of star?",
+    answers: {
+      a: "White Neutron Star",
+      b: "Red Supergiant Star",
+      c: "Blue Supergiant Star",
+      d: "Supernova"
+    },
+    correct: "b"
+  },
+  {
+    question: "What is the gas and dust called that follows a comet?",
+    answers: {
+      a: "Tail",
+      b: "Debris field",
+      c: "Spray",
+      d: "Wake"
+    },
+    correct: "a"
+  },
+  {
+    question: "What is the proper name of the sun?",
+    answers: {
+      a: "Solus",
+      b: "Solara",
+      c: "Sol",
+      d: "Salsa"
+    },
+    correct: "c"
+  },
+  {
+    question: "Which planet is also known as the Morning Star?",
+    answers: {
+      a: "Mercury",
+      b: "Jupiter",
+      c: "Saturn",
+      d: "Venus"
+    },
+    correct: "d"
+  },
+  {
+    question: "Which planet is also known as the Evening Star?",
+    answers: {
+      a: "Venus",
+      b: "Sirius",
+      c: "Jupiter",
+      d: "Mercury"
+    },
+    correct: "a"
+  },
+  {
+    question: "What is the point of no return around a black hole called?",
+    answers: {
+      a: "The point of no return",
+      b: "Event Horizon",
+      c: "Terminal Boundary",
+      d: "Stellar Cusp"
+    },
+    correct: "b"
+  },
+  {
+    question: "How much time does it take for the sun's light to reach earth?",
+    answers: {
+      a: "3 minutes",
+      b: "5 minutes",
+      c: "10 minutes",
+      d: "8 minutes"
+    },
+    correct: "d"
+  },
+  {
+    question:
+      "What is the point called when the moon is furthest from the Earth?",
+    answers: {
+      a: "Apex",
+      b: "Apogee",
+      c: "Perigee",
+      d: "Apsis"
+    },
+    correct: "b"
+  },
+  {
+    question: "Which planet spins backwards relative to the rest?",
+    answers: {
+      a: "Uranus",
+      b: "Jupiter",
+      c: "Venus",
+      d: "Saturn"
+    },
+    correct: "c"
+  }
+];
+
+// variables
+var currentQuestion;
+var currentScore = 0;
+
+//functions
+function initButtons() {
+  //initialize button functionality
+  //RESTART.addEventListener("click", function() {location.startGame();});
+  RESTART.addEventListener("click", function() {
+    location.reload();
+  });
+  // yes okay that last part to reload the page is a HOTFIX.
+  // is that okay? I ran into a bug that I tore my hair out trying to fix
+  // long story short, I couldn't fix it so I applied this as a hotfix.
+  // it's not elegant, I am not proud of it, but it works.
+  // simply starting the game and reinitializing the variables causes the game to crash on subsequent playthroughs...
+  RESTART.classList.add("hidden");
+  SCORECONT.classList.add("hidden");
+  CHOICEA.addEventListener("click", function() {
+    checkAnswer("a");
+  });
+  CHOICEB.addEventListener("click", function() {
+    checkAnswer("b");
+  });
+  CHOICEC.addEventListener("click", function() {
+    checkAnswer("c");
+  });
+  CHOICED.addEventListener("click", function() {
+    checkAnswer("d");
+  });
+}
+
+function displayQuestion(q) {
+  //takes in the current question object
+  //displays question and choices
+  QUESTION.innerHTML = q.question;
+  CHOICEA.innerHTML = q.answers.a;
+  CHOICEB.innerHTML = q.answers.b;
+  CHOICEC.innerHTML = q.answers.c;
+  CHOICED.innerHTML = q.answers.d;
+}
+
+function nextQuestion() {
+  currentQuestion++;
+  if (currentQuestion < QUESTIONS.length) {
+    displayQuestion(QUESTIONS[currentQuestion]);
+  } else {
+    endGame();
+  }
+}
+
+function checkAnswer(a) {
+  //takes the button that is clicked
+  //compares to current questions correct answer
+  //updates score
+  //calls nextQuestion()
+  if (a === QUESTIONS[currentQuestion].correct) {
+    console.log("Correct!");
+    currentScore++;
+  } else {
+    console.log("WRONG");
+  }
+  nextQuestion();
+}
+
+function endGame() {
+  SCORE.innerHTML = currentScore;
+  RESTART.classList.remove("hidden");
+  SCORECONT.classList.remove("hidden");
+}
+
+function startGame() {
+  currentQuestion = 0;
+  currentScore = 0;
+  initButtons();
+  displayQuestion(QUESTIONS[currentQuestion]);
+}
+
+startGame();
